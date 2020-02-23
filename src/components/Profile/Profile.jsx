95a2ab0e248531c3bc81fe52e2383d1a37cd33e1
Sergey1888888 <Sergey1888888@gmail.com>
2020-02-23 17:09:12 +0400

    test

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import s from './Profile.module.css'
-import MyPosts from './MyPosts/MyPosts';
-import ProfileInfo from './ProfileInfo/ProfileInfo';
+import React from "react";
+import s from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
-    return (
+  return (
     <div>
       <ProfileInfo />
       <MyPosts />
-    </div>);
-}
+    </div>
+  );
+};
 
 export default Profile;
