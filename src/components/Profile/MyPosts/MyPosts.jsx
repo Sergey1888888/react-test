6604a79af1128855eebe009cd8fad403d628de04
Sergey1888888 <Sergey1888888@gmail.com>
2020-02-23 16:54:02 +0400

    test

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import s from './MyPosts.module.css'
-import Post from './Post/Post';
+import React from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 
 const MyPosts = () => {
+  let posts = [
+    { id: 1, message: "Hi, how are you?", likesCount: 13 },
+    { id: 2, message: "It's my first post!", likesCount: 24 }
+  ];
 
-    let posts = [
-      {id: 1, message: "Hi, how are you?", likesCount: 13},
-      {id: 2, message: "It's my first post!", likesCount: 24}
-    ]
+  let postsElements = posts.map(p => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
-    let postsElements = posts
-                      .map(p => <Post message={p.message} likesCount={p.likesCount} /> );
-
-    return (
+  return (
     <div className={s.posts_block}>
       <h3>My posts</h3>
       <div>
@@ -23,11 +23,9 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        {postsElements}
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
-    )
-}
+  );
+};
 
 export default MyPosts;
