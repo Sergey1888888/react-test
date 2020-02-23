dd2ed8d5f4c765faec73ac28c9571bc720edc6e7
Sergey1888888 <Sergey1888888@gmail.com>
2020-02-23 17:05:38 +0400

    test commit

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import s from './Post.module.css'
+import React from "react";
+import s from "./Post.module.css";
 
-const Post = (props) => {
-    return (
-      <div className={s.item}>
-        <img src="https://i1.sndcdn.com/avatars-000477535869-mq50ss-t500x500.jpg"/>
-        <div className={s.message}>
-          {props.message} {props.likesCount} Likes
-        </div>
+const Post = props => {
+  return (
+    <div className={s.item}>
+      <img src="https://i1.sndcdn.com/avatars-000477535869-mq50ss-t500x500.jpg" />
+      <div className={s.message}>
+        {props.message} {props.likesCount} Likes
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Post;
