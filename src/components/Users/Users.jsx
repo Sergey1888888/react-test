8143aa535d9c1bf8dd6129bf0d979a40d4ceef1e
Sergey1888888 <sergey1888888@google.com>
2020-06-26 20:28:02 +0400

    style + axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,52 +1,34 @@
 import React from "react";
 import s from "./Users.module.css";
+import * as axios from "axios";
+import defaultUserPhoto from "./../../assets/images/user.png";
 
 let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: "https://i.redd.it/tyhrngisvqv31.png",
-                followed: false,
-                fullName: "Sergey",
-                status: "Я бог",
-                location: { city: "Moscow", country: "Russia" },
-            },
-            {
-                id: 2,
-                photoUrl:
-                    "https://i.ytimg.com/vi/gRm57CMI28Y/hqdefault_live.jpg",
-                followed: true,
-                fullName: "Dmitry",
-                status: "Я бог",
-                location: { city: "Volgograd", country: "Russia" },
-            },
-            {
-                id: 3,
-                photoUrl: "https://cdn.frankerfacez.com/emoticon/390750/4",
-                followed: false,
-                fullName: "Vitya",
-                status: "Я бог",
-                location: { city: "Harkiv", country: "Ukraine" },
-            },
-            {
-                id: 4,
-                photoUrl: "https://i.redd.it/qm3ksi2yajq41.png",
-                followed: false,
-                fullName: "Maks",
-                status: "Я бог",
-                location: { city: "Minsk", country: "Belarus" },
-            },
-        ]);
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios
+                .get("https://social-network.samuraijs.com/api/1.0/users")
+                .then((response) => {
+                    props.setUsers(response.data.items);
+                });
+        }
     }
 
     return (
         <div className={s.flex}>
+            <button onClick={getUsers}>Get users</button>
             {props.users.map((u) => (
                 <div key={u.key} className={s.flex_wrapper}>
                     <div className={s.flex_avatarbtn}>
                         <div>
-                            <img src={u.photoUrl} className={s.userPhoto} />
+                            <img
+                                src={
+                                    u.photos.small != null
+                                        ? u.photos.small
+                                        : defaultUserPhoto
+                                }
+                                className={s.userPhoto}
+                            />
                         </div>
                         <div>
                             {u.followed ? (
@@ -72,12 +54,12 @@ let Users = (props) => {
                     </div>
                     <div className={s.flex_info}>
                         <div className={s.flex_left_info}>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </div>
                         <div className={s.flex_right_info}>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </div>
                     </div>
                 </div>
