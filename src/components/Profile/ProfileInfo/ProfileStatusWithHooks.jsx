5e915f4567225fa38266e5464135eb7db8053840
sergey1888888 <sergey1888888@gmail.com>
2020-08-30 14:47:13 +0400

    useEffect

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import s from "./ProfileInfo.module.css";
+import { useState, useEffect } from "react";
+
+const ProfileStatusWithHooks = (props) => {
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status);
+    
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    };
+    
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        if (status !== props.status) {
+            props.updateStatus(status);
+        }
+    };
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    };
+
+    return (
+        <div>
+            {editMode ? (
+                <div>
+                    <input
+                        onChange={onStatusChange}
+                        autoFocus={true}
+                        onBlur={deactivateEditMode}
+                        value={status}
+                    />
+                </div>
+            ) : (
+                <div>
+                    <span onDoubleClick={activateEditMode}>
+                        {props.status ? props.status : "Нет статуса"}
+                    </span>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ProfileStatusWithHooks;
